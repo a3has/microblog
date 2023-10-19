3983d596ef50acf2d3a18addbd7e2244f4756b94
kiloGram <abed3hasbini@gmail.com>
2023-10-19 11:06:10 -0700

    proper formatting

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,11 @@ from app import app
 from flask import render_template
 from datetime import datetime
 
+
 @app.route('/')
-
-
 @app.route('/index')
 def index():
     admin = {'username': 'Aboudi'}
     users = {'Mike', 'Moe'}
-    
+
     return render_template('index.html', admin=admin, datetime=datetime.now(), users=users)
